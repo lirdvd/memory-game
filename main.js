ac53e441a699198cb5593e9fe45f6053418cac6c
lirdvd <lirandvd@gmail.com>
2021-12-07 17:48:29 +0200

    new save

--- main.js
+++ main.js
@@ -2,49 +2,65 @@ const cards = [
   { name: "A" },
   { name: "B" },
   { name: "C" },
+  { name: "D" },
+  { name: "E" },
+  { name: "F" },
+  { name: "G" },
+  { name: "H" },
+  { name: "I" },
+  { name: "J" },
+  { name: "K" },
+  { name: "L" },
   { name: "A" },
   { name: "B" },
   { name: "C" },
+  { name: "D" },
+  { name: "E" },
+  { name: "F" },
+  { name: "G" },
+  { name: "H" },
+  { name: "I" },
+  { name: "J" },
+  { name: "K" },
+  { name: "L" },
 ];
 
-function shuffle(arr) {
-    
+function randRange(a, b) {
+  return Math.floor(a + Math.random() * (b - a));
 }
 
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
 
 function shuffle(arr) {
-    let currentIndex = arr.length,  randomIndex;
-    while (currentIndex != 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [arr[currentIndex], arr[randomIndex]] = [
-        arr[randomIndex], arr[currentIndex]];
-    }
-  
-    return arr;
-  }
-
-
-for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 100; i++) {
     let a1 = randRange(0, arr.length);
     let a2 = randRange(0, arr.length);
     if (a1 == a2) {
-        1--;
-        continue;
+      i--;
+      continue;
     }
-    swap(arr, a1, a2)
+    swap(arr, a1, a2);
+  }
 }
 
+let check = (arr, i, j) => i != j && arr[i].name === arr[j].name;
 
-function check(arr, i, j) {
-    return arr[i].name == arr[j].name && i != j
+function createCardElement(card) {
+  const board = document.getElementById("board");
+  const cardEl = document.createElement("div");
+  cardEl.innerHTML = card.name;
+  cardEl.className = "card";
+  board.appendChild(cardEl);
 }
-
 
 function main() {
-    console.log([...cards]);
-    shuffle(cards);
-    console.log(cards);
+  for (let card of cards) {
+    createCardElement(card);
+  }
 }
 
 main();
